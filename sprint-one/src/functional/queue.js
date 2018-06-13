b4846655e5f99b37ac2679631525e565f0b3ace3
Hanjoon <hanjoon3@hotmail.com>
2018-06-13 12:27:21 -0700

    Implement stack and queue in functional instantation foramt

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,26 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var last = 1;
+  var first = 1
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    someInstance[last] = value;
+    storage = JSON.parse(JSON.stringify(someInstance));
+    last++;
   };
 
   someInstance.dequeue = function() {
+    if (someInstance.size() > 0) {
+      storage = JSON.parse(JSON.stringify(someInstance));
+      delete someInstance[first]
+      first++;
+      return storage[first -1];
+    }
   };
 
   someInstance.size = function() {
+    return last - first;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
 var Stack = function() {
+  // someInstance will hold the last pushed or popped key/value
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  // storage will hold the stack
   var storage = {};
 
+  var key = 1;
   // Implement the methods below
   someInstance.push = function(value) {
-  };
+    someInstance[key] = value;
+    storage = JSON.parse(JSON.stringify(someInstance));
+    key++;
+  }
 
   someInstance.pop = function() {
+    if (someInstance.size() > 0) {
+      storage = JSON.parse(JSON.stringify(someInstance));
+      delete someInstance[key];
+      key--;
+      return storage[key];
+    }
   };
 
   someInstance.size = function() {
+    return key - 1;
   };
 
   return someInstance;
