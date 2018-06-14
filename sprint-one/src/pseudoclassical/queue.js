560f726b49469930affa27b97512a4a1b096a14f
hanjoon-kim <hanjoon3@gmail.com>
2018-06-13 17:51:07 -0700

    Implement through pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,5 +1,26 @@
 var Queue = function() {
-  var someInstance = function()
+  this.someInstance = {};
+  this.storage = {};
+  this.first = 1;
+  this.last = 1;
+
 };
 
+  Queue.prototype.enqueue = function(value) {
+    this[this.last] = value;
+    this.storage = JSON.parse(JSON.stringify(this));
+    this.last++;
+  };
 
+  Queue.prototype.dequeue = function() {
+    if (this.size() > 0) {
+      this.storage = JSON.parse(JSON.stringify(this));
+      delete this[this.first]
+      this.first++;
+      return this.storage[this.first -1];
+    };
+  }
+
+  Queue.prototype.size = function () {
+    return this.last - this.first;
+  };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.someInstance = {};
+  this.storage = {};
+  this.key = 1;
 };
 
+Stack.prototype.push = function(value) {
+  this[this.key] = value;
+  this.storage = JSON.parse(JSON.stringify(this));
+  this.key++;
+};
 
+Stack.prototype.pop = function() {
+  if (this.size() > 0) {
+    this.storage = JSON.parse(JSON.stringify(this));
+    delete this[this.key];
+    this.key--;
+    return this.storage[this.key];
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.key - 1;
+}
