feaf523c4d4893946c62d2586bd84832a04bdb3a
hanjoon-kim <hanjoon3@gmail.com>
2018-06-14 11:07:17 -0700

    Implement through linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,43 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //check if head is null
+    if (list.head === null) {
+      //instantiate head with value
+      list.head = Node(value);
+      list.tail = list.head;
+      return;
+    }
+    //point to next new Node
+    list.tail.next = Node(value);
+    list.tail = list.tail.next;
   };
 
   list.removeHead = function() {
+    //if null, error
+    if (list.head === null) {
+      return;
+    }
+    var previous = list.head.value;
+    list.head = list.head.next;
+    return previous;
+    //point list.head to the head.next node
+
+    
   };
 
   list.contains = function(target) {
+    //set helper variable to equal list.head
+    var finder = list.head; 
+    //check head, if head.next exists, check, recurse through it
+    while (finder !== null) {
+      if (finder.value === target) {
+        return true;
+      }
+      finder = finder.next;
+    }
+    return false;   
   };
-
   return list;
 };
 
